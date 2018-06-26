cc03339ca18713d7cffee70cf93645fe7e5d142d
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2018-06-27 00:39:44 +0200

    Cleanup of List_length.c

--- finalexamples/List_length.c
+++ finalexamples/List_length.c
@@ -1,49 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <malloc.h>
-#include <math.h>
+#include <stddef.h>
 
-
-
-
-
-
-//The following predicates are auto generated 
-/*@ 
-
-predicate node (struct node *node; int count) = 
- node == 0 ? count == 0 : node->data |-> ?data &*& node->key |-> ?key &*& node->next |-> ?next &*& malloc_block_node(node)  &*& node(next, ?count1) &*& count == count1 + 1; 
-
-
-@*/
-
-
-struct node  {      char data;   int key;   
+struct node {
+   int key;
+   char data;
    struct node *next;
 };
 
+// TODO: Generate a postcondition that describes the return value.
 
-
-
-//****************************************************************************************************************************
-
-
-// return the length of a linked list
-void length(struct node* head, int length)
-//@requires true;
-//@ensures true;
+int length(struct node *head)
+//@ requires true;
+//@ ensures true;
 {
-   if(head == 0){abort();}
-   length = 0;
-   struct node *current;
-   for(current = head; current != NULL; current = current->next)
-   //@requires true;
-   //@ensures true;
+   int length = 0;
+   for (struct node *current = head; current != NULL; current = current->next)
+   //@ requires true;
+   //@ ensures true;
    {
       length++;
    }
+   return length;
 }
 
 
